ba3edc1ac9eb011b880e09c5ee548a9798536eb2
chris514591 <117721748+chris514591@users.noreply.github.com>
2023-03-18 22:38:01 +0100

    VICTORY
    Save generated password to file and bind to either randomly generated user or specified user with -=user=USERNAMEHERE.

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func generatePassword(passLength int) string {
 var (
 	length = flag.Int("length", 12, "password length")
 	count  = flag.Int("count", 1, "number of passwords to generate")
+	user   = flag.String("user", "", "user name")
 )
 
 func main() {
@@ -42,13 +43,31 @@ func main() {
 		log.Fatalf("invalid password count: %d", *count)
 	}
 
+	if *user == "" {
+		*user = fmt.Sprintf("user_%d", time.Now().Unix())
+	}
+
+	f, err := os.OpenFile("test123.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
 	if *length == 12 {
 		for i := 0; i < *count; i++ {
 			password := generatePassword(*length)
+			line := fmt.Sprintf("%s: %s\n", *user, password)
+			if _, err := f.WriteString(line); err != nil {
+				log.Fatal(err)
+			}
 			fmt.Println(password)
 		}
 	} else {
 		password := generatePassword(*length)
+		line := fmt.Sprintf("%s: %s\n", *user, password)
+		if _, err := f.WriteString(line); err != nil {
+			log.Fatal(err)
+		}
 		fmt.Println(password)
 	}
 }
